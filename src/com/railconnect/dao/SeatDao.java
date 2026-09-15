0d32568f6d6ae7950ba0367f13dd93ddb58d1539
ankit-birgade27 <ankitbirgade@gmail.com>
2026-09-15 10:51:40 +0530

    removing dublicate code

--- src/com/railconnect/dao/SeatDao.java
+++ src/com/railconnect/dao/SeatDao.java
@@ -9,46 +9,6 @@ import com.railconnect.model.Seat;
 import com.railconnect.model.Coach;
 import com.railconnect.model.Train;
 
-public class SeatDao {
-	
-	   private List<Seat> seats = new ArrayList<>();
-	   
-	   private TrainDao trainDao;
-
-	    public SeatDao(TrainDao trainDao) {
-	        this.trainDao = trainDao;
-	    }
-
-	    public boolean trainExists(int trainId) {
-	        return trainDao.findById(trainId) != null;
-	    }
-
-	  public Seat findByTrainIdAndSeatNumber(int trainId, String seatNumber) {
-		  Train train = trainDao.findById(trainId);
-
-	        if (train == null) {
-	            return null;
-	        }
-
-	        if (train.getCoaches() == null) {
-	            return null;
-	        }
-
-	        for (Coach coach : train.getCoaches()) {
-
-	            if (coach.getSeats() == null) {
-	                continue;
-	            }
-
-	            for (Seat seat : coach.getSeats()) {
-
-	                if (seat.getSeatNumber() != null &&
-	                    seat.getSeatNumber().equals(seatNumber)) {
-
-	                    return seat;
-	                }
-	            }
-	        }
 
 import java.util.List;
 
@@ -79,9 +39,10 @@ public class SeatDao {
 				   seats.set(i, seat);
 			   }
 		   }
-	   }
+	   
 
 
-		   return null;
+		  
+		  
 	   }
 }
